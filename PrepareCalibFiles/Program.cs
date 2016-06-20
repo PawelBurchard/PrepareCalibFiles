e6194455c1a0bb010ffd56200e13b7bd895414b9
PawelBurchard <pawel.burchard1@gmail.com>
2016-06-20 13:46:07 +0200

    I'm throwin' it down yeah

---
+++ PrepareCalibFiles/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace editFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Studia\Hokuyo-Kinect\Pictures"; //insert write path to files
+            DirectoryInfo d = new DirectoryInfo(path);
+            FileInfo[] infos = d.GetFiles();
+            long num = 1465679400;
+            using (StreamWriter sw = File.CreateText(path + @"\laser.txt"))
+            {
+                foreach (FileInfo f in infos)
+                {
+                    if (f.FullName.IndexOf("laser") != -1)
+                    {
+                        using (StreamReader sr = File.OpenText(f.FullName))
+                        {
+
+                            string s = sr.ReadLine(); 
+                            string s2 = "";
+                            s = s.Remove(0, 12);
+                            string sDist = s.Remove(0, 34);
+
+                            string[] distances = sDist.Split(',');
+                            string pyk;
+                            for (int i = 0; i < 256; i++)
+                            {
+                                pyk = distances[510 - i];
+                                distances[510 - i] = distances[i];
+                                distances[i] = pyk;
+
+                            }
+                            for (int i = 0; i < 511; i++)
+                            {
+                                if (i != 510)
+                                    s2 = s2 + distances[i] + ',';
+                                else
+                                    s2 = s2 + distances[i];
+                            }
+                            s = s.Insert(0, num.ToString());
+                            s = s.Substring(0, 43);
+                            s = s + s2;
+                            sw.WriteLine(s);
+                            num += 10;
+                        }
+                    }
+                }
+            }
+            num = 1465679400;
+            using (StreamWriter sw2 = File.CreateText(path + @"\camera.txt"))
+            {
+                foreach (FileInfo f in infos)
+                {
+                    if (f.FullName.IndexOf("camera") != -1)
+                    {
+                        using (StreamReader sr = File.OpenText(f.FullName))
+                        {
+                            string s = sr.ReadLine();
+                            sw2.WriteLine(num.ToString());
+                            num += 10;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ PrepareCalibFiles/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PrepareCalibFiles")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("PrepareCalibFiles")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5ded1cd7-7177-4993-8d07-5b7d7651cb31")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
